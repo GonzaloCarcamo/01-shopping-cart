8db957877596119e8652c9f775d45190c61ae465
goncmodev <gon.cmo.dev@gmail.com>
2021-06-04 12:58:39 +0100

    Update courses quantity

--- js/app.js
+++ js/app.js
@@ -37,8 +37,22 @@ function leerDatosCurso(curso){
                 cantidad: 1
         }
 
-        // Agrega elementos al arreglo del carrito
-        articulosCarrito = [...articulosCarrito, infoCurso];
+        const existe = articulosCarrito.some( curso => curso.id === infoCurso.id );
+        if(existe){
+                // Actualizamos la cantidad
+                const cursos = articulosCarrito.map( curso => {
+                        if( curso.id === infoCurso.id ){
+                                curso.cantidad++;
+                                return curso; // Retorna el objeto actualizado
+                        } else {
+                                return curso; // Retorna los elementos no duplicados
+                        }
+                });
+                articulosCarrito = [...cursos];
+        } else {
+                // Agrega elementos al arreglo del carrito
+                articulosCarrito = [...articulosCarrito, infoCurso];
+        }
 
         console.log(articulosCarrito);
 
